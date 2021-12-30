2e873bdce2b98cae8b86486cde80d107484ef9de
KirillWebDeveloper404 <kirillcernysev148@gmail.com>
2021-12-30 12:33:32 +0300

    rework client panel

--- static/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.menu-burger__header').click(function() {
-        $('.menu-burger__header').toggleClass('open-menu');
-        $('.header__menu').toggleClass('header__menu_active');
-    });
-  });
